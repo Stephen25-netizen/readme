1b63743f41c1460343f04f6cab6a0a714b032c35
Stephen25-netizen <stevohsmart@gmail.com>
2025-04-17 08:55:32 +0300

    initial commit with project files

--- components/Profile.js
+++ components/Profile.js
@@ -46,7 +46,7 @@ function Profile() {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
                                 </svg>
                             </span>
-                            <span className="text-gray-600">0746644085</span>
+                            <span className="text-gray-600">+254 0746644085</span>
                         </div>
                         <div className="flex items-center gap-3 group cursor-pointer" data-name="email">
                             <span className="flex items-center justify-center w-10 h-10 rounded-lg bg-pink-100 text-pink-600 transition-colors group-hover:bg-pink-200">
